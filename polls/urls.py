587c0457910ec443eb014c9af5c42fd7b1451f8e
knarimatsu <nariomasahisa@gmail.com>
2023-12-24 00:58:50 +0900

    chore: add comments

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # poll配下のviewsのindexを実行
     path("", views.index, name="index"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    print(request)
     return HttpResponse("Hello, world. You are at polls index.")
