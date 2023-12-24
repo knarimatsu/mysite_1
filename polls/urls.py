90ee91a94b2066b4131a4d705dcef6f6110af65e
knarimatsu <nariomasahisa@gmail.com>
2023-12-24 23:52:15 +0900

    pollアプリケーションのurl.pyにurl patternを追加

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 
+"""_summary_
+第1引数:URIパターンを記載
+第2引数:該当するURIがリクエストできた時に実行するviewのメソッド
+第3引数(name=の部分):任意、名前つけるだけ
+"""
+
 urlpatterns = [
-    # poll配下のviewsのindexを実行
     path("", views.index, name="index"),
+    path("<int:question_id>", views.detail, name="detail"),
+    path("<int:question_id/result", views.results, name="results"),
+    path("<int:question_id/vote>", views.vote, name="vote"),
 ]
