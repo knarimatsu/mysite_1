b1f29211bbe7a78ffec43e6e74963e33014bb02b
knarimatsu <nariomasahisa@gmail.com>
2023-12-25 00:41:57 +0900

    templateに反映できるようにresponseを修正

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
@@ -12,8 +13,10 @@ def index(request):
         _type_: Questionで新しいものから5個ほど取り出し返す
     """
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    # return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
