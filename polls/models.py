0a0cc4dd6e557b59693349407892d8bef048568c
knarimatsu <nariomasahisa@gmail.com>
2023-12-24 15:08:21 +0900

    モデルの各クラスにオブジェクトを文字列に変換する関数の追加

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 # Questionテーブル
 class Question(models.Model):
+    def __str__(self) -> str:
+        return self.question_text
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
@@ -10,6 +13,9 @@ class Question(models.Model):
 # Choiceテーブル
 # 外部キーquestion
 class Choice(models.Model):
+    def __str__(self) -> str:
+        return self.choice_text
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
