a46b544675fe854c40ab13fa7a42cd6cd7da08be
knarimatsu <nariomasahisa@gmail.com>
2023-12-25 23:55:27 +0900

    fix:views.detail method(不要なコード削除)

--- polls/views.py
+++ polls/views.py
@@ -13,9 +13,7 @@ def index(request):
         _type_: Questionで新しいものから5個ほど取り出し返す
     """
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
-    # return HttpResponse(template.render(context, request))
     return render(request, "polls/index.html", context)
 
 
