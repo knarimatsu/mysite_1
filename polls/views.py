c25c4bbe88ebd558d19b8fd9bc88e683815ed8fa
knarimatsu <nariomasahisa@gmail.com>
2023-12-25 23:33:36 +0900

    fix: views.detail method (Djangoが用意しているショートカットのライブラリを使用)

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import loader
 
@@ -27,10 +27,7 @@ def detail(request, question_id):
     Returns:
         _type_: 選択されたQuestionの詳細が何かを表示してくれる
     """
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
