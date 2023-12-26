209ea56b3d6e2117b3841a5b115e034c4ab72b7d
knarimatsu <nariomasahisa@gmail.com>
2023-12-26 21:52:02 +0900

    fix:polls/views.py(Http4004の代わりにget_object_or_404を利用)

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from django.template import loader
 
 from .models import Question
