4e27f35771e9b798c39b6188465033d376904818
knarimatsu <nariomasahisa@gmail.com>
2023-12-31 00:47:35 +0900

    feat: add test code

--- polls/tests.py
+++ polls/tests.py
@@ -11,7 +11,20 @@ class QuestionModelTests(TestCase):
         """
         未来日が公開日となっているQuestionはFalseを返すことを確認する
         """
-
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_with_old_question(self):
+        """ """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_with_recent_question(self):
+        """
+        公開日が1日前の場合はTrueを返す
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
